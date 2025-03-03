3b7da05fc974848e2b89be50b8641ec9af33cbc4
co-devclub <aarambh2k24.3.o@gmail.com>
2025-03-03 09:04:52 +0530

    Update my.js

--- my.js
+++ my.js
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const centerXName = (pageWidth - textWidthName) / 2;
 
         firstPage.drawText(name, {
-            x: 100,
+            x: 50,
             y: 310,
             size: 30,
             font: MTCORSVA,
